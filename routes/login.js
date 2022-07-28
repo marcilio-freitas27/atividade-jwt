32ef4b6c487c0de4caa8084da73abe524fff0297
Marcílio Freitas <marbass18@gmail.com>
2022-07-28 16:12:56 -0300

    Integração com api-jwt.

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,19 @@
 require("dotenv-safe").config();
 const jwt = require('jsonwebtoken');
 const express = require('express'); 
-const app = express(); 
+const app = express();
+const cors = require('cors');
 
 const password = '12345';
+
+app.use(cors());
 
 app.post('/login', (req, res, next) => {
     if(req.body.user === 'luiz' && req.body.pwd === '123'){
         const id = 1; 
-        const token2 = jwt.sign({ id }, password);
+        // const token2 = jwt.sign({ id }, password);
         const token = jwt.sign({ id }, process.env.SECRET);
-        return res.json({ auth: true, token: token , token2: token2});
+        return res.json({ auth: true, token: token});
     }
 
     res.status(500).json({message: 'Login inválido!'});
