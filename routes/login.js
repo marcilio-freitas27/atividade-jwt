0fd9bf8a0ff1373fc66a09aa7cb1cd7cffcc9e91
Marcílio Freitas <marbass18@gmail.com>
2022-08-04 16:15:41 -0300

    Alterando a chave do token.

--- routes/login.js
+++ routes/login.js
@@ -11,8 +11,8 @@ app.use(cors());
 app.post('/login', (req, res, next) => {
     if(req.body.user === 'luiz' && req.body.pwd === '123'){
         const id = 1; 
-        // const token2 = jwt.sign({ id }, password);
-        const token = jwt.sign({ id }, process.env.SECRET);
+        const token = jwt.sign({ id }, password);
+        // const token = jwt.sign({ id }, process.env.SECRET);
         return res.json({ auth: true, token: token});
     }
 
